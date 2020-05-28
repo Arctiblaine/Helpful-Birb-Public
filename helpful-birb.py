209ef2af8cf65d0c60b46ee8c5a69a051cd9e03d
Arctiblaine <snowyleopard799@gmail.com>
2020-05-28 12:29:27 -0700

    forgot to handle numbers for hb!bigtext

--- helpful-birb.py
+++ helpful-birb.py
@@ -623,7 +623,17 @@ alphabet = {'a':':regional_indicator_a:',
             'w':':regional_indicator_w:',
             'x':':regional_indicator_x:',
             'y':':regional_indicator_y:',
-            'z':':regional_indicator_z:'}
+            'z':':regional_indicator_z:',
+            '1':'clock1',
+            '2':'clock2',
+            '3':'clock3',
+            '4':'clock4',
+            '5':'clock5',
+            '6':'clock6',
+            '7':'clock7',
+            '8':'clock8',
+            '9':'clock9',
+            '0':'clock12'}
 
 @bot.command()
 async def bigtext(ctx, *, phrase):
